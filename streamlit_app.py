d4412225e3a2a004888f754e286370a0d6cb9f51
Shenghan.Zhang <shenghan.zhang1123@gmail.com>
2024-09-23 12:32:30 -0400

    Completed Problem 1 & 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -115,8 +115,7 @@ chart = alt.Chart(subset).mark_rect().encode(
     x=alt.X('Age:O', sort=ages),
     y='Country:O',
     color=alt.Color('Rate:Q', scale=alt.Scale(
-            domain=[subset['Rate'].min(), subset['Rate'].max()],
-            scheme='viridis'
+            domain=[subset['Rate'].min(), subset['Rate'].max()]
         ), legend=alt.Legend(title="Mortality rate per 100k"))
 ).properties(
     title=f"{cancer} mortality rates for {'males' if sex == 'M' else 'females'} in {year}"
