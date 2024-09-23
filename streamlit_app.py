ebf0528a2d8ffe5e777b48af161467034635796d
Shenghan.Zhang <shenghan.zhang1123@gmail.com>
2024-09-23 12:52:21 -0400

    Completed Problem 1 & 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -120,6 +120,7 @@ chart = alt.Chart(subset).mark_rect().encode(
         alt.Color('Rate:Q', scale=alt.Scale(
             type='log', base=10,
             domain=[0.01, 100],
+            scheme='blues'
         ), legend=alt.Legend(title="Mortality rate per 100k")
                   ),
     )
